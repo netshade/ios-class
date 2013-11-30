9c1fa1be0caf6e06c1cb74a77f4006e0b342d563
Chris <christopher@fastestforward.com>
2013-11-30 10:04:15 -0500

    update lab3

---
+++ labs/Lab3/Lab3/PCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PCAppDelegate.h
+//  Lab3
+//
+//  Created by Chris Zelenak on 11/29/13.
+//  Copyright (c) 2013 Bootstrapping iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ labs/Lab3/Lab3/PCDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  PCDetailViewController.m
+//  Lab3
+//
+//  Created by Chris Zelenak on 11/29/13.
+//  Copyright (c) 2013 Bootstrapping iOS. All rights reserved.
+//
+
+#import "PCDetailViewController.h"
+
+@interface PCDetailViewController ()
+
+@end
+
+@implementation PCDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
