02bea0438746cb4a2b558226a63c81ee716bf4db
Chris <christopher@fastestforward.com>
2013-12-03 11:03:37 -0500

    add lab 8

---
+++ labs/Lab8/Lab8/PCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PCTableViewController.h
+//  Lab8
+//
+//  Created by Chris Zelenak on 12/3/13.
+//  Copyright (c) 2013 Bootstrapping iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PCTableViewController : UITableViewController
+
+@end
---
+++ labs/Lab8/Lab8/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Lab8
+//
+//  Created by Chris Zelenak on 12/3/13.
+//  Copyright (c) 2013 Bootstrapping iOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PCAppDelegate class]));
+    }
+}
